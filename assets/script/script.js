1ec9611a0fffd0d07b8f4eb2cf8601a66385b26a
Ahmed Sajjad <uasajjad11198@outlook.com>
2021-08-29 13:08:14 -0400

    added prompt and window.confirms for criteria and pushed those values to an object

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,44 @@
 // Assignment code here
+function generatePassword() {
+  // ask user for length of password
+  var promptLength = window.prompt("Please choose the length of your password. Enter a value between 8 characters and 128 characters long.");
+  // if input is less than 8 or greateer than 128 return generatePassword()
+  if (promptLength < 8 || promptLength > 128){
+    window.alert("You need to provide a valid answer! Please try again.");
+    return generatePassword();
+  }
+  // ask user for password criteria
+  var confirmLowerCase = window.confirm("Would you like lower case letters to be included in your password?");
+  var confirmUpperCase = window.confirm("Would you like upper case letters to be included in your password?");
+  var confirmNumeric = window.confirm("Would you like numbers to be included in your password?");
+  var confirmSpecialChar = window.confirm("Would you like special characters to be included in your password?");
+  // password criteria object
+  var confirmOptions = {
+    length: promptLength,
+    lowercase: confirmLowerCase,
+    uppercase: confirmUpperCase,
+    numeric: confirmNumeric,
+    specialchar: confirmSpecialChar
+  };
 
+  var optionsArray = []
+
+  // if statements to push options to an array
+  if (confirmLowerCase === true) {
+    //logic to push to array
+  }
+  if (confirmUpperCase === true) {
+    //logic to push to array
+  }
+  if (confirmNumeric === true) {
+    //logic to push to array
+  }
+  if (confirmSpecialChar === true) {
+    //logic to push to array
+  }
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
